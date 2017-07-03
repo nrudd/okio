dae9b758909eff5f746198ad75801fde9990b12c
Nate Rudd <nrudd@crexendo.com>
2017-07-03 11:37:10 -0700

    remove diamond arguments and set jdk to 1.6

--- okio/src/test/java/okio/ByteStringTest.java
+++ okio/src/test/java/okio/ByteStringTest.java
@@ -577,7 +577,7 @@ public final class ByteStringTest {
         factory.decodeHex("fe"),
         factory.decodeHex("ff"));
 
-    List<ByteString> sortedByteStrings = new ArrayList<>(originalByteStrings);
+    List<ByteString> sortedByteStrings = new ArrayList<ByteString>(originalByteStrings);
     Collections.shuffle(sortedByteStrings, new Random(0));
     Collections.sort(sortedByteStrings);
 
@@ -613,7 +613,7 @@ public final class ByteStringTest {
         factory.decodeHex("7f0000ffff"),
         factory.decodeHex("ffffff"));
 
-    List<ByteString> sortedByteStrings = new ArrayList<>(originalByteStrings);
+    List<ByteString> sortedByteStrings = new ArrayList<ByteString>(originalByteStrings);
     Collections.shuffle(sortedByteStrings, new Random(0));
     Collections.sort(sortedByteStrings);
 
